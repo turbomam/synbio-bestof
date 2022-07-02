88665ae80e06ae929faa70c1f4425fada69faebe
Mark A. Miller <mamillerpa@gmail.com>
2022-07-02 00:07:10 -0400

    ready for worthiness?

--- src/synbio-bestof/datamodel/synbio_bestof.py
+++ src/synbio-bestof/datamodel/synbio_bestof.py
@@ -1,5 +1,5 @@
 # Auto generated from synbio_bestof.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-07-01T13:05:43
+# Generation date: 2022-07-01T13:42:12
 # Schema: synbio
 #
 # id: http://example.com/synbio
